0015c58d1743e58eef9eb803e266e142d6ef1b73
COMLAB <hniguidula23-0360@cca.edu.ph>
2025-02-26 19:55:09 +0800

    UPDATE
    Update

--- register.php
+++ register.php
@@ -7,15 +7,19 @@
 </head>
 <body>
 <form action="action_page.php" method="POST">
-    <table border="1">
+    <table border="1" cellpadding="20" cellspacing="5" width="60%">
+        <tr>
+            <td> <b><i>QUESTION</i></b> </td>
+            <td> <b><i>ANSWER</i></b> </td>
+        </tr>
         <tr>
             <td> <b><i>Name:</i></b> </td>
-            <td> <input type="text" name="name"> </td>
+            <td> <input type="text" name="name" value="Halden"> </td>
         </tr>
         <tr>
             <td> <b><i>Gender:</i></b> </td>
             <td>
-                <input type="radio" name="gender" value="male"> <b><i>Male</i></b>
+                <input type="radio" name="gender" value="male" checked> <b><i>Male</i></b> <br></br>
                 <input type="radio" name="gender" value="female"> <b><i>Female</i></b>
             </td>
         </tr>
@@ -23,15 +27,16 @@
             <td> <b><i>Country:</i></b> </td>
             <td>
                 <select name="country">
-                    <option value="PH">Philippines</option>
+                    <option value="Select Country">--SELECT COUNTRY--</option>
+                    <option value="PH" selected>Philippines</option>
                     <option value="AUS">Australia</option>
                     <option value="US">United States</option>
                 </select>
             </td>
         </tr>
         <tr>
-            <td colspan="2">
-                <input type="submit" value="Save">
+            <td colspan="2" align="center">
+                <input type="submit" name="submit" value="Save">
             </td>
         </tr>
     </table>
