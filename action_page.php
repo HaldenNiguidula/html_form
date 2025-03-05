94f8b0cc5931d1b9aaaaa3c481df8bbf191c2bb3
COMLAB <hniguidula23-0360@cca.edu.ph>
2025-03-05 18:18:33 +0800

    Added Database
    ARAWWW KUPU!!!

--- action_page.php
+++ action_page.php
@@ -14,7 +14,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "wait_lang_sir4";
+$database = "wait_lang_sir";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -26,7 +26,7 @@ echo "Connected successfully";
 ?>
 
 <?php
-$sql = "INSERT INTO wait_lang_sir4 (id, full_name, gender, country)
+$sql = "INSERT INTO student_record (id, fullname, gender, country)
 VALUES (Null, '$fullname', '$gender', '$country')";
 
 if ($conn->query($sql) === TRUE) {
